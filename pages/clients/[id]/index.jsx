772e0f16f628f85299d51a070860bb7d581ea379
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-03 00:03:38 +0530

    navigate dynamically

--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -1,7 +1,14 @@
+import { useRouter } from 'next/router'
+
 const index = () => {
+  const router = useRouter()
+  const loadProjectHandler = () => {
+    router.push('/clients/max/sfd')
+  }
   return (
     <div>
       <h1>Client Page</h1>
+      <button onClick={loadProjectHandler}>Load Projects</button>
     </div>
   )
 }
